1ec5bcf529de6dfe5238be578a9a7ab14b18072f
Mohan Pradhan <manojpradhan628@gmail.com>
2022-05-03 13:49:47 +0545

    contact post url changes

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -90,10 +90,7 @@ const Contact = () => {
 
       const sendMail = async () => {
         try {
-          const res = await axios.post(
-            "https://my-react-portfolio-backend.herokuapp.com/contact/",
-            values
-          );
+          const res = await axios.post("http://localhost:5000/contact", values);
           if (res) {
             toast.success("Message Sent!", {
               autoClose: 2500,
